6a8d5d3f85aa9308265ad264e4e789e824dc067e
buvale <valentina.butto@gmail.com>
2018-05-02 11:05:50 -0400

    this is a new package

---
+++ R/f.R
@@ -0,0 +1,13 @@
+#'  TEST
+#'A little test to see if you are able
+#'
+#' @param x this is the indipendent variable
+#' @param y this is the dependent variable
+#'
+#' @return
+#' @export
+#'
+#' @examples
+f <- function(x, y) {
+  x + y
+}
